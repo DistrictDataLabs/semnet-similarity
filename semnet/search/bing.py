d1baafbb21c73fc506d1b724ed031d34b70bec17
Rebecca Bilbro <bilbro@gmail.com>
2015-05-20 20:56:26 -0400

    bing search implemented

--- semnet/search/bing.py
+++ semnet/search/bing.py
@@ -19,39 +19,66 @@ Implements Bing API Search
 #############################################################
 
 import requests
-import string
+import os
 from base import BaseSearch
 
 #############################################################
 ## Bing Search API
 #############################################################
 
+def quote(s):
+    return "'%s'" % s.strip("\"'")
+
 class BingSearch(BaseSearch):
     """
     Implements the Bing search API.
     """
 
-    HOST = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Composite?"
+    HOST = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Composite"
     
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, key, format='json', top=10, skip=0):
+        self.key    = key
+        self.top    = top
+        self.skip   = skip
+        self.format = format
+
         
-    def search(self, sources, query, params):
-    	"""
-    	Search for the query on Bing
-    	"""
-    	request = 'Sources=%27' + sources + '%27'
-    	request += '&Query=%27' + str(query) + '%27'
-    	for key,value in params.iteritems():
-    		request += '&' + key + '=' + str(value) 
-        request = self.HOST + request
-        return requests.get(request, auth=(self.key, self.key))
+    def search(self, sources, query, **params):
+        """
+        Search for the query on Bing
+        """
+        data = {
+            'Sources' : quote(sources),
+            'Query' : quote(query),
+        }
+        
+        data.update(self.parse_default_params(params))
+        response=requests.get(self.HOST, params=data, auth=(self.key, self.key))
+
+        response.raise_for_status()
+        return response
+    
+    def parse_default_params(self, params):
+        defaults = {
+            'format' : self.format,
+            'top': self.top,
+            'skip': self.skip
+        }
+        
+        defaults.update(params)
+        return dict(("$" + key, val) for key,val in defaults.items())
+    
+    def paginated_search(self, sources, query, **params):
+        while True:
+            yield self.search(sources, query, **params)
+            self.skip = self.skip + self.top        
+
+
 
 if __name__ == "__main__":
-	my_key = raw_input("Enter your API key: ")
-	query_string = raw_input ("Enter your search terms: ")
-	bing = BingSearch(my_key)
-	params = {'$format': 'json',
-			  '$top': 10,  # change to 1000
-			  '$skip': 0}
-	print bing.search('web',query_string,params).json()
+    my_key = os.environ["BING_API_KEY"]
+    query_string = "bread and honey"
+    bing = BingSearch(my_key)
+    print bing.search('web',query_string,top=20).json()
+    
+    
