a0f310a464b9b7a1141b04970cdb57a1f0b93243
Rebecca Bilbro <bilbro@gmail.com>
2015-05-30 11:04:11 -0400

    small updates to bing.py

--- semnet/search/bing.py
+++ semnet/search/bing.py
@@ -13,13 +13,12 @@
 Implements Bing API Search
 """
 
-
 #############################################################
 ## Imports
 #############################################################
 
 import requests
-import os
+import os   # to access API key
 from base import BaseSearch
 
 #############################################################
@@ -27,13 +26,17 @@ from base import BaseSearch
 #############################################################
 
 def quote(s):
+    """
+    strip single and double quotation marks from a string,
+    then add single quotation marks to format for Bing
+    """
     return "'%s'" % s.strip("\"'")
 
 class BingSearch(BaseSearch):
     """
     Implements the Bing search API.
     """
-
+    
     HOST = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Composite"
     
     def __init__(self, key, format='json', top=10, skip=0):
@@ -41,7 +44,6 @@ class BingSearch(BaseSearch):
         self.top    = top
         self.skip   = skip
         self.format = format
-
         
     def search(self, sources, query, **params):
         """
@@ -49,36 +51,39 @@ class BingSearch(BaseSearch):
         """
         data = {
             'Sources' : quote(sources),
-            'Query' : quote(query),
+            'Query'   : quote(query),
         }
         
-        data.update(self.parse_default_params(params))
-        response=requests.get(self.HOST, params=data, auth=(self.key, self.key))
+        data.update(self.parse_default_params(params)) # merge params into data dictionary
+        response=requests.get(self.HOST, params=data, auth=(self.key, self.key)) # send request
 
-        response.raise_for_status()
-        return response
+        response.raise_for_status() 
+        return response # return REST status if error
     
     def parse_default_params(self, params):
+        """
+        Take default params and format them for Bing
+        """
         defaults = {
             'format' : self.format,
-            'top': self.top,
-            'skip': self.skip
+            'top'    : self.top,
+            'skip'   : self.skip,
         }
         
         defaults.update(params)
         return dict(("$" + key, val) for key,val in defaults.items())
+ 
     
-    def paginated_search(self, sources, query, **params):
-        while True:
-            yield self.search(sources, query, **params)
-            self.skip = self.skip + self.top        
-
+#   ## Eventually we will want a function to handle multiple pages of results
+#
+#   def paginated_search(self, sources, query, **params):
+#       while True:
+#           yield self.search(sources, query, **params)
+#           self.skip = self.skip + self.top        
 
 
 if __name__ == "__main__":
-    my_key = os.environ["BING_API_KEY"]
+    my_key = os.environ["BING_API_KEY"] # key stored in os path
     query_string = "bread and honey"
     bing = BingSearch(my_key)
-    print bing.search('web',query_string,top=20).json()
-    
-    
+    print bing.search('web', query_string).json()
